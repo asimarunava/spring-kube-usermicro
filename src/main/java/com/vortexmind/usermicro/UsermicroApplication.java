ba119b5cc8980c4562251a8e4f7172dac01de331
asim <asimarunava@gmail.com>
2020-11-23 19:01:00 +0530

    initial commit.

---
+++ src/main/java/com/vortexmind/usermicro/UsermicroApplication.java
@@ -0,0 +1,13 @@
+package com.vortexmind.usermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vortexmind/usermicro/UsermicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexmind.usermicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
