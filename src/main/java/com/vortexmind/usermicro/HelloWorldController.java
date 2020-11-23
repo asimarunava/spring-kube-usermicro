0ffb20a5a1ef8fc750bce1be9cb1a0332e56750a
asim <asimarunava@gmail.com>
2020-11-23 19:18:51 +0530

    adding deployment yaml

---
+++ src/main/java/com/vortexmind/usermicro/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.vortexmind.usermicro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String hello() {
+        return "This is UserMicro";
+    }
+}
