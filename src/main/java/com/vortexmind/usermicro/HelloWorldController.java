4d9c1852023277ea0dfb8663733db93df82c4dbd
asim <asimarunava@gmail.com>
2020-11-28 15:29:32 +0530

    Changing callee to get data from servicename and full qualified name

--- src/main/java/com/vortexmind/usermicro/HelloWorldController.java
+++ src/main/java/com/vortexmind/usermicro/HelloWorldController.java
@@ -4,12 +4,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
 
     @GetMapping
-    public String hello() {
-        return "This is UserMicro inside cluster";
+    public String hello() throws IOException {
+
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("http://mymdbmicro-service:8080/movies");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
+    @GetMapping("/new")
+    public String hellonew() throws IOException {
+
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("http://mymdbmicro-service.default.svc.cluster.local:8080/movies");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString()+"from new url";
+    }
+
 }
